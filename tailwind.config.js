bb3a488882b7c64ee05946251f7706990d88d561
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-15 00:21:32 +0100

    build(config): add converter function 🧱

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,25 @@ const colors = require("@radix-ui/colors");
 /** @type {import("tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
   content: ["index.html"],
+  theme: { colors: radixToTailwindConfig() },
 };
+
+function radixToTailwindConfig() {
+  const initial = { current: "currentColor", transparent: "transparent" };
+
+  return Object.entries(colors).reduce((acc, [color, colorSteps]) => {
+    const arr = color.split(/(?=[A-Z])/);
+    const steps = Object.entries(colorSteps).reduce(
+      (acc, [step, value]) => ({
+        ...acc,
+        [step.replace(new RegExp(`${arr[0]}a?`, "i"), "")]: value,
+      }),
+      {}
+    );
+
+    return {
+      ...acc,
+      [arr.join("-").toLowerCase()]: steps,
+    };
+  }, initial);
+}
