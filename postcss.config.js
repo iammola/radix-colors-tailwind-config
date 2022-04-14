9a7833a90697d550c6733d6e69c433eeea5b9bcb
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-15 00:11:21 +0100

    build(config): tailwind init ⚒️

---
+++ postcss.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  postcss: {},
+  tailwindcss: {},
+  autoprefixer: {},
+};
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+const colors = require("@radix-ui/colors");
+
+/** @type {import("tailwindcss/tailwind-config").TailwindConfig } */
+module.exports = {
+  content: ["index.html"],
+};
