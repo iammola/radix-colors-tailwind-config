665fd1f21c135055a2348c586a14f81c4ba69ff7
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-15 23:31:22 +0100

    build(tailwindcss): add white and black to initial colors 🎨

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,12 @@ function getStep(step, color) {
 }
 
 function radixToTailwindConfig() {
-  const initial = { current: "currentColor", transparent: "transparent" };
+  const initial = {
+    white: "white",
+    black: "black",
+    current: "currentColor",
+    transparent: "transparent",
+  };
 
   return Object.entries(colors).reduce((acc, [color, colorSteps]) => {
     const arr = color.split(/(?=[A-Z])/);
