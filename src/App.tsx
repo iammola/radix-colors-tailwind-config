271bd2ad0e3071d4b836f3b081a714bd779ff9b4
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-15 23:22:16 +0100

    feat!: remove `useTheme` hook 🎨

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { Fragment, useRef } from "react";
+import { Fragment } from "react";
 
 import Section from "./Section";
-import { useTheme } from "./hooks";
 import { BrightColors, Colors, Grays, Metals, Overlays } from "./data";
 
 const App: React.FC = () => {
-  const ref = useRef<HTMLElement>(null);
-  useTheme(ref);
-
   return (
-    <main ref={ref} className="bg-mauve-1 py-10 px-40 font-medium">
+    <main className="bg-mauve-1 py-10 px-40 font-medium">
       <h1 className="text-4xl text-slate-12">The Scales</h1>
       <p className="text-slate-11">
         An overview of all 30 Radix Colors scales.
