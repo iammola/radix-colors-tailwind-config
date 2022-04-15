1b0ec050d3dcc4fb46fc00bbd8761e87e097e002
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-15 10:49:50 +0100

    refactor(app): render scales at heading level ♻️

--- src/Color.tsx
+++ src/Color.tsx
@@ -1,25 +1,25 @@
 const Color: React.FC<ColorProps> = ({ color, scales }) => {
   return (
-    <div className="space-y-[5px] mt-[45px]">
-      <h3 className="text-slate-12 text-[17px] capitalize empty:hidden">
-        {color}
-      </h3>
-      <div className="space-y-[2px]">
-        <div className="flex items-center justify-center gap-x-[2px]">
-          <div className="grow" />
-          {Array.from({ length: 12 }, (_, i) => (
-            <span
-              key={i}
-              className="inline-block w-[50px] text-center text-slate-11"
-            >
-              {i + 1}
-            </span>
-          ))}
-        </div>
-        {Object.entries(scales).map(([scale, steps]) => (
-          <Scale key={scale} {...{ scale, steps }} />
+    <div className="space-y-[2px] mt-[45px]">
+      {color && (
+        <h3 className="text-slate-12 text-[17px] capitalize pb-[3px]">
+          {color}
+        </h3>
+      )}
+      <div className="flex items-center justify-center gap-x-[2px]">
+        <div className="grow" />
+        {Array.from({ length: 12 }, (_, i) => (
+          <span
+            key={i}
+            className="inline-block w-[50px] text-center text-slate-11"
+          >
+            {i + 1}
+          </span>
         ))}
       </div>
+      {Object.entries(scales).map(([scale, steps]) => (
+        <Scale key={scale} {...{ scale, steps }} />
+      ))}
     </div>
   );
 };
