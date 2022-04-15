756a3ab1f915d2b1313819c3a06021c7d01afdd4
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-15 03:23:29 +0100

    feat(app): add title ✨

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  return (
+    <main className="bg-mauve-1 space-y-4 font-medium">
+      <h1 className="text-slate-12 text-4xl">The Scales</h1>
+      <p className="text-slate-11">
+        An overview of all 30 Radix Colors scales.
+      </p>
+    </main>
+  );
 };
 
 export default App;
