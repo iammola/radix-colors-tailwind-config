9d4852ce580d4461676fc3be29c5f0cf731c3e72
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-15 04:44:18 +0100

    feat(app): add color sections 💪

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+import { Fragment } from "react";
+
+import Section from "./Section";
+import { BrightColors, Colors, Grays, Metals, Overlays } from "./data";
+
 const App: React.FC = () => {
   return (
     <main className="bg-mauve-1 space-y-4 font-medium">
@@ -5,6 +10,49 @@ const App: React.FC = () => {
       <p className="text-slate-11">
         An overview of all 30 Radix Colors scales.
       </p>
+      <Section
+        title="Colors"
+        colors={Colors}
+        description={
+          <Fragment>
+            Radix Colors provides 15 color scales, designed for white foreground
+            text at step{" "}
+            <code className="text-violet-11 bg-violet-3 text-[max(12px,_85%)]">
+              9
+            </code>
+            .
+          </Fragment>
+        }
+      />
+      <Section
+        title="Bright Colors"
+        colors={BrightColors}
+        description={
+          <Fragment>
+            Radix Colors provides 5 bright scales, designed for black foreground
+            text at step{" "}
+            <code className="text-violet-11 bg-violet-3 text-[max(12px,_85%)]">
+              9
+            </code>
+            .
+          </Fragment>
+        }
+      />
+      <Section
+        title="Grays"
+        colors={Grays}
+        description="Radix Colors provides 6 gray scales."
+      />
+      <Section
+        title="Metals"
+        colors={Metals}
+        description="Radix Colors provides 2 metal scales."
+      />
+      <Section
+        title="Overlays"
+        colors={{ Overlays }}
+        description="The black and white scales are designed for overlays. These scales don’t change across themes."
+      />
     </main>
   );
 };
